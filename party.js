26e7df02bea86689455cb00ca5ed2e8effab4d89
dnoce3 <dnoce3@gatech.edu>
2025-06-10 21:12:53 -0400

    expanded party list

--- party.js
+++ party.js
@@ -1,8 +1,98 @@
-
 let parties = new Map();
-
-parties.set("dnoce.fl@gmail.com", ["Derek Noce", "Kimberly Noce", "Luke Noce", "Holly Noce"]);
+parties.set("kris.aneu@outlook.com", ["Jim Jenkins", "Kristi Jenkins", "Kaden Jenkins", "Sam Jenkins"]);
+parties.set("tripalshah@gmail.com", ["Nitin Shah", "Tripal Shah"]);
+parties.set("rowanmuhl@gmail.com", ["Rowan Muhl"]);
+parties.set("rachana26@msn.com", ["Minesh Zaveri", "Rachana Zaveri", "Riya Zaveri", "Karina Zaveri", "Dylan Zaveri", "Nina Zaveri", "Sarah Che", "David Che"]);
+parties.set("ysche74@gmail.com", ["Young Che", "Jane Che"]);
+parties.set("watyre@ccim.net", ["Warren Tyre", "Mary Margaret Tyre"]);
+parties.set("sweetymansi9@gmail.com", ["Sweety Rana", "Harsh Rana", "Child Rana"]);
+parties.set("nayanaashah@hotmail.com", ["Anil Shah", "Nayana Shah"]);
+parties.set("vavg@gmail.com", ["Nelson Norris"]);
+parties.set("crgrams@aol.com", ["Carolyn Grams"]);
+parties.set("etjv@bellsouth.net", ["Quint Johnson"]);
+parties.set("jshah385@gmail.com", ["Jay Shah", "Nikisha Shah", "Amir Shah"]);
+parties.set("rick.fowler@stjohns.k12.fl.us", ["Rick Fowler", "Christine Fowler"]);
+parties.set("jldcpa36@gmail.com", ["Joe D'Ambrosio", "Joe's Guest"]);
+parties.set("cjstrunk@bex.net", ["Crawford Strunk", "Shelly Strunk", "Asa Strunk", "Emma Strunk", "Everett Strunk"]);
+parties.set("rshah1624@gmail.com", ["Amita Shah", "Vishal Shah", "Dev Shah", "Ba Shah", "Dada Shah"]);
+parties.set("hemal7681@gmail.com", ["Hemal Shah", "Alka Shah", "Ba Shah", "Dada Shah", "Jason Oswald"]);
+parties.set("bela.bordner@gmail.com", ["Sam Califano", "Isabela Bordner"]);
+parties.set("dnoce.fl@gmail.com", ["Derek Noce", "Kimberly Noce", "Holly Noce", "Luke Noce"]);
+parties.set("jaime.lieber@gmail.com", ["Hal Lieber", "Jaime Lieber", "Eliana Lieber", "Stuart Lieber"]);
+parties.set("lululogan@gmail.com", ["Robert Grams"]);
+parties.set("ashah10@gmail.com", ["Amol Shah", "Jenita Shah", "Maya Shah"]);
+parties.set("fdeprospo@me.com", ["Frank DeProspo", "Nicole DeProspo"]);
+parties.set("canzz55@hotmail.com", ["James Canzoneri", "Kathleen Canzoneri", "James Canzoneri Jr.", "Judson Canzoneri", "Joseph Canzoneri"]);
+parties.set("anlachanna@embarqmail.com", ["Laurie O'Hara"]);
+parties.set("jstoddart73@hotmail.com", ["Mark Stoddart", "Jennifer Stoddart"]);
+parties.set("chudgars@yahoo.com", ["Siren Chudgar"]);
+parties.set("dawnlc2012@gmail.com", ["Derek Cunningham", "Dawn Cunningham"]);
+parties.set("clay.9.carter@gmail.com", ["Clay Carter", "Presley Carter"]);
+parties.set("minesh.rkc@gmail.com", ["Minesh Shah", "Minesh's Guest"]);
+parties.set("sunnyandnikita91@gmail.com", ["Sunny Shah", "Nikita Shah"]);
+parties.set("jkaley@aol.com", ["James Kaley", "Janet Kaley"]);
+parties.set("birenshah9@gmail.com", ["Biren Shah", "Dhruvika Shah", "Child Shah", "Child Shah"]);
+parties.set("tookernc@att.net", ["Russ Tooke", "Caren Tooke"]);
+parties.set("gmagloff@comcast.net", ["Glen Magloff", "Cindy Magloff"]);
 parties.set("dagnynoce@gmail.com", ["Dagny Noce", "Vishay Chudgar"]);
+parties.set("r1b1gr2@aol.com", ["Ron Graves", "Betty Graves", "Zack Graves", "Zeanna Graves"]);
+parties.set("sabastianfallon@gmail.com", ["Sabastian Fallon"]);
+parties.set("amshah1@gmail.com", ["Atit Shah", "Lina Shah", "Aria Shah", "Ava Shah"]);
+parties.set("shoemakerconsulting@gmail.com", ["Matt Shoemaker", "Matt's Guest", "Child Shoemaker", "Child Shoemaker"]);
+parties.set("nbertoni314@gmail.com", ["Nick Bertoni", "Brandi Bertoni", "Nicholas Bertoni", "William Bertoni"]);
+parties.set("suzy_rankin@hotmail.com", ["Suzanne Rankin Hall", "Nicholas Hall"]);
+parties.set("alison@haashome.net", ["Adam Demeter", "Ali Haas"]);
+parties.set("shah.prima@ymail.com", ["Chintu Shah", "Prima Shah", "Jainim Shah", "Nandani Patel"]);
+parties.set("tommandt@gmail.com", ["Tom Mandt", "Holly Mandt"]);
+parties.set("anishi98@gmail.com", ["Riyaz Dhruv", "Anishi Dhruv", "Child Dhruv", "Child Dhruv"]);
+parties.set("askshah@aol.com", ["Sandeep Shah", "Kashmira Shah", "Arti Shah", "Akash Shah", "Ba Shah", "Dada Shah"]);
+parties.set("kumarsanjay.analytics@gmail.com", ["Sanjay Kumar"]);
+parties.set("tobinbossola@gmail.com", ["Tobin Bossola", "Rachel Bossola"]);
+parties.set("arnik.shah@gmail.com", ["Arnik Shah", "Bhavini Shah", "Minaxi Shah", "Child Shah", "Child Shah"]);
+parties.set("nanbev@aol.com", ["Nan O'Hara"]);
+parties.set("mihica.desai@gmail.com", ["Chintan Desai", "Guddi Desai", "Child Desai", "Child Desai"]);
+parties.set("becky.j.grams@gmail.com", ["Becky Grams", "Nick Grams"]);
+parties.set("mgscheid@mtco.com", ["Charles Schied", "MaryGail Schied"]);
+parties.set("eblucier@att.net", ["Bill Lucier", "Erica Lucier"]);
+parties.set("specklesz@aol.com", ["Karl Zacek", "Vera Zacek"]);
+parties.set("jennifermoran73@yahoo.com", ["Jennifer Chudgar"]);
+parties.set("nickwcalifano@gmail.com", ["Nick Califano", "Morgan Califano"]);
+parties.set("sangita1014@yahoo.com", ["Vijay Shah", "Sangita Shah"]);
+parties.set("lydiabell13@gmail.com", ["Lydia Bell", "Lawrence Bell"]);
+parties.set("arzaveri82@gmail.com", ["Ami Burow", "Matt Burow", "Child Burow", "Child Burow"]);
+parties.set("hshah802@yahoo.com", ["Hiren Shah", "Nilam Shah"]);
+parties.set("sophiaacalifano@gmail.com", ["Sophia Califano"]);
+parties.set("egrams99@gmail.com", ["Eric Grams", "Melissa Grams", "Makenna Grams", "Harper Grams", "Logan Grams"]);
+parties.set("ljlantgios@gmail.com", ["Larry Lantgios"]);
+parties.set("ginnymoran@sbcglobal.net", ["Mark Woerishoffer", "Virginia Woerishoffer"]);
+parties.set("lucascalifano0905@gmail.com", ["Lucas Califano", "Abigail Yazdia"]);
+parties.set("akspshah@gmail.com", ["Ajay Shah", "Kalpana Shah", "Pinky Shah", "Sonali Shah", "Mohit Jani"]);
+parties.set("kkreyling@gmail.com", ["Steve Kreyling", "Kathy Kreyling"]);
+parties.set("julie.grams@comcast.net", ["Norman Grams", "Julie Grams"]);
+parties.set("itisashleyhere@gmail.com", ["Ashley Werner", "Missy Werner"]);
+parties.set("al.bertoni@aol.com", ["Al Bertoni", "Denise Bertoni"]);
+parties.set("vcroberts0131@gmail.com", ["Ayden Rebhan", "Cassidy Roberts"]);
+parties.set("mansishah17@gmail.com", ["Dhaval Choski", "Mansi Choski", "Child Choski", "Child Choski"]);
+parties.set("cshaan1234@gmail.com", ["Shaan Chudgar"]);
+parties.set("laurens9124@gmail.com", ["Lauren Sonnier"]);
+parties.set("jaymin.m.kothari@gmail.com", ["Jaymin Kothari", "Paloma Kothari", "Surekha Kothari", "Child Kothari", "Child Kothari"]);
+parties.set("bonnieruckh@yahoo.com", ["Gene Ruckh", "Bonnie Ruckh", "Brandon Ruckh", "Max Ruckh", "Danielle Ruckh", "Aaron Ruckh"]);
+parties.set("jchudgar0215@gmail.com", ["Jatin Chudgar", "Hemisha Chudgar", "Mansi Chudgar", "Kajol Chudgar", "Mohit Pandya"]);
+parties.set("crumb86@hotmail.com", ["Anita Azam", "Anita's Guest"]);
+parties.set("pgoldpt@gmail.com", ["Phil Goldsmith", "Stephanie Goldsmith", "Nathan Goldsmith"]);
+parties.set("sandy.oswald321@gmail.com", ["Sandra Oswald", "Arthur Oswald"]);
+parties.set("lepugs@gmail.com", ["Le Tran", "Le's Guest"]);
+parties.set("yojanashah@gmail.com", ["Nandu Shah", "Yojana Shah"]);
+parties.set("jatinshah1974@icloud.com", ["Jatin Shah", "Mittal Shah"]);
+parties.set("vakil59@gmail.com", ["Jashwant Shah", "Smita Shah"]);
+parties.set("dimplekandarp@gmail.com", ["Kandarp Shah", "Dimple Shah", "Anaya Shah", "Kiara Shah"]);
+parties.set("dipjaggu@yahoo.com", ["Jigar Shah", "Dipali Shah", "Aadi Shah", "Caitlin Kenney", "Dharendra Shah", "Pratima Shah"]);
+parties.set("meyerwilliby@gmail.com", ["Adam Williby", "Bree Williby"]);
+parties.set("phim.long13@gmail.com", ["Phil Long", "Madeline Lewis"]);
+parties.set("nilshah@optonline.net", ["Nil Shah", "Alison Triolo", "Aurelia Triolo", "Wyatt Triolo", "Althea Triolo"]);
+parties.set("sgrams1112@gmail.com", ["Jeremiah King", "Stacy King", "Everly King"]);
+parties.set("shahcacpa@yahoo.com", ["Mayank Shah", "Smita Shah"]);
+
 
 
 
@@ -36,7 +126,7 @@ function searchChart() {
     // Clear error message
     document.getElementById('errorMsg').style.display = "none";
     
-    document.getElementById('party').innerText = "WE HAPPILY INVITE";
+    document.getElementById('party').innerText = "WE JOYFULLY INVITE";
     document.getElementById('party').style.display = "block";
     document.getElementById('line').style.display = "block";
 
